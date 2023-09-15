5c4d76865bbf5da2e57b06d4fbde4afec1c79542
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2023-09-15 23:31:42 +0100

    diff two arrays algo

---
+++ Intermediate/js/diff_two_arrays.js
@@ -0,0 +1,12 @@
+function diffArray(arr1, arr2) {
+  return arr1
+    .concat(arr2)
+    .filter((item) => !arr1.includes(item) || !arr2.includes(item));
+}
+
+console.log(
+  diffArray(
+    ["andesite", "grass", "dirt", "pink wool", "dead shrub"],
+    ["diorite", "andesite", "grass", "dirt", "dead shrub"]
+  )
+);
